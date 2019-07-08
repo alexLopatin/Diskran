0235fc89445a8036d2c0cc46f93a84c0d699bfb6
alexLopatin <minelabory@gmail.com>
2019-07-08 23:19:08 +0300

    added jokers

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -2,34 +2,47 @@
 using namespace std;
 
 int TBohr::GetSuffLink(int v){
-    if (Bohr[v].suffLink==-1) //если еще не считали
-        if (v==0||Bohr[v].par==0) //если v - корень или предок v - корень
-            Bohr[v].suffLink=0;
-        else
-            Bohr[v].suffLink=GetAutoMove(GetSuffLink(Bohr[v].par), Bohr[v].symb);
+    if (Bohr[v].suffLink == -1) //если еще не считали
+        if (v==0||Bohr[v].par == 0) { //если v - корень или предок v - корень
+            Bohr[v].suffLink = 0;
+        }
+        else {
+            Bohr[v].suffLink = GetAutoMove(GetSuffLink(Bohr[v].par), Bohr[v].symb);
+        }
     return Bohr[v].suffLink;
 }
-int TBohr::GetAutoMove(int v, char ch){
-    if (Bohr[v].autoMove[ch]==-1)
-        if (Bohr[v].nextVertex[ch]!=-1)
-            Bohr[v].autoMove[ch]=Bohr[v].nextVertex[ch];
-        else
-            if (v==0)
-                Bohr[v].autoMove[ch]=0;
-            else
-                Bohr[v].autoMove[ch]=GetAutoMove(GetSuffLink(v), ch);
-    return Bohr[v].autoMove[ch];
+int TBohr::GetAutoMove(int v, string s){            // метод, реализующий действие ДКА
+    auto autoPair = Bohr[v].autoMove.find(s);
+    auto nextPair = Bohr[v].nextVertex.find(s);
+    if (Bohr[v].autoMove.count(s) == 0) {
+        if (Bohr[v].nextVertex.count(s) != 0){
+            autoPair->second = nextPair->second;
+        }
+        else{
+            if (v == 0) {
+                autoPair->second = 0;
+            }
+            else {
+                autoPair->second = GetAutoMove(GetSuffLink(v), s);
+            }
+        }
+    }
+       
+    autoPair = Bohr[v].autoMove.find(s);
+    return autoPair->second;
 }
 TBohr::TBohr(const string& s) {
     Pattern = s;
-    Bohr.push_back(TBohrVertex(0, '#'));
+    Bohr.push_back(TBohrVertex(0, "#"));
 
-    // split string with delimiter '?'
+    // разбиваем строку на несколько с делителем '?'
     string str = s;
     string delimiter = "?";
     size_t pos = 0;
+    int jokers = 1;
+    int i = -1;
     string token;
-    while ((pos = str.find(delimiter)) != std::string::npos) {
+    while ((pos = str.find(delimiter)) != string::npos) {
         char t, l = 0;
         if((str.size()-1) != pos) {
             if(str[pos+1] == ' ') {
@@ -41,69 +54,106 @@ TBohr::TBohr(const string& s) {
                 l = 1;
             }
         }
-        token = str.substr(0, pos-l);
-        cout << token << endl;
-        AddString(token);
-        str.erase(0, pos + delimiter.length()+t);
-    }
-    cout << str << endl;
-    AddString(str);
-}
-void TBohr::AddString(const string& s) {
-    int num = 0;
-
-    for (int i = 0; i < s.length(); i++) {
-        char ch = s[i] - 'a';
-        if( Bohr[num].nextVertex[ch] == -1) {
-            Bohr.push_back(TBohrVertex(num,ch));
-            Bohr[num].nextVertex[ch] = Bohr.size()-1;
+        token = str.substr(0, pos - l);
+        if(token.size() > 0) {
+            AddString(token);
+            jokerCount.push_back(1);
+            i++;
         }
-        num = Bohr[num].nextVertex[ch];
+        else {
+            jokerCount[i]+=1;
+        }
+        str.erase(0, pos + delimiter.length() + t);
     }
-    
+    if(str.size() > 0) {
+        AddString(str); // Разбили строку-образец на несколько строк, не содержащих джокеров
+    }
+}
+void TBohr::AddString(const string& s) {   // добавляет строку в бор
+    int num = 0;
+    TStringIterator* it = new TStringIterator(s);
+    while(!it->End()) {
+        string str = it->Next();
+        if( Bohr[num].nextVertex.count(str) == 0) {
+            Bohr.push_back(TBohrVertex(num,str));
+            Bohr[num].nextVertex.insert(pair<string, int>(str,Bohr.size() - 1));
+        }
+        num = Bohr[num].nextVertex.find(str)->second;
+    }
     Bohr[num].flag = true;
     PatternSplit.push_back(s);
     Bohr[num].patternNumber = PatternSplit.size() - 1;
+    delete it;
 }
-bool TBohr::IsStringInBohr(const string& s) {
-int num = 0;
-for (int i = 0; i < s.length(); i++) {
-    char ch = s[i] - 'a';
-    if( Bohr[num].nextVertex[ch] == -1) {
-        return false;
-    }
-    num = Bohr[num].nextVertex[ch];
-}
-return true;
-}
-int TBohr::GetSuffFLink(int v){ 
-   if (Bohr[v].suffFLink==-1){
+int TBohr::GetSuffFLink(int v) {      // циклический переход по суффиксным ссылкам, пока не будет ссылаться на конечную вершину
+   if (Bohr[v].suffFLink == -1) {
       int u = GetSuffLink(v);
-      if (u==0) //либо v - корень, либо суф. ссылка v указывает на корень 
-         Bohr[v].suffFLink=0;
+      if (u == 0)                     // либо v - корень, либо суф. ссылка v указывает на корень 
+         Bohr[v].suffFLink = 0;
       else
-         Bohr[v].suffFLink=(Bohr[u].flag)?u:GetSuffFLink(u);
+         Bohr[v].suffFLink = (Bohr[u].flag) ? u : GetSuffFLink(u);
    }
    return Bohr[v].suffFLink;
 }
 
-void TBohr::FindAllPos(const string& s){
-    int u=0;
-    for(int i=0;i<s.length();i++){
-        u=GetAutoMove(u,s[i]-'a');
-        Check(u,i+1);
+void TBohr::FindAllPos(const string& s){         // сам алгоритм, находит вхождения каждого подобразца в отдельности и потом проверяет
+    int u = 0;                                   // их относительный порядок
+    TStringIterator* it = new TStringIterator(s);
+    int i = 0;
+    while(!it->End()) {
+        string str = it->Next();
+        u = GetAutoMove(u,str);;
+        for(int v = u;v != 0;v = GetSuffFLink(v)) {
+            if (Bohr[v].flag) {
+                Positions.push_back(pair<string, int>(PatternSplit[Bohr[v].patternNumber], i - PatternLength(Bohr[v].patternNumber) + 2));
+            }
+        }
+        i++;
     }
-}
-void TBohr::Check(int v,int i){
-    for(int u=v;u!=0;u=GetSuffFLink(u)){
-        if (Bohr[u].flag) 
-            cout<<i-PatternSplit[Bohr[u].patternNumber].length()+1<<" "<<PatternSplit[Bohr[u].patternNumber]<<endl;
+    delete it;
+    for ( int i = 0; i < Positions.size(); i++ )            // проверка относительного порядка
+    {
+        if(CheckPosition(i, 0)) {
+            std::cout << Pattern 
+                  << ':'
+                  << Positions[i].second   
+                  << std::endl;
+        }
     }
+    
 }
-
+bool TBohr::CheckPosition(int positionIndex, int patternIndex) {            // метод рекурсивной проверки относительного порядка
+    bool res = false;
+    if(Positions[positionIndex].first == PatternSplit[patternIndex]) {
+        if(patternIndex == PatternSplit.size()-1) {
+            return true;
+        }
+        if(positionIndex == Positions.size()-1) {
+            return false;
+        }
+        int i = 1;
+        while(Positions[positionIndex + i].second - PatternLength(patternIndex) - Positions[positionIndex].second <= jokerCount[patternIndex]) {
+            res = res || CheckPosition(positionIndex + i, patternIndex + 1);
+            i++;
+            if(positionIndex + i > Positions.size() - 1) {
+                break;
+            }
+        }    
+    }
+    return res;
+}
+int TBohr::PatternLength(int index) {
+    TStringIterator* it = new TStringIterator(PatternSplit[index]);
+    int i = 0;
+    while(!it->End()) {
+        it->Next();
+        i++;
+    }
+    return i;
+}
 
 int main(int argc, char *argv[]) {
-    TBohr* bohr = new TBohr("cat goat ? dog ? lamp nose airplane ? go ons");
-    
+    TBohr* bohr = new TBohr("cat ? dog ? cow");
+    bohr->FindAllPos("cat cat dog dog cow");
     delete bohr;
 }
--- lab4/lab4.h
+++ lab4/lab4.h
@@ -2,42 +2,78 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 
-const unsigned char ALPHABET_SIZE = 26;
-
-struct  TBohrVertex {
-    int nextVertex[ALPHABET_SIZE];
-    int suffLink;
-    int suffFLink;
-    int autoMove[ALPHABET_SIZE];
-    int par;
-    bool flag;
-    char symb;
-    int patternNumber;
-    TBohrVertex(int p, char c) {
-        memset(nextVertex, 255, sizeof(nextVertex));
-        memset(autoMove, 255, sizeof(autoMove));
-        flag = false;
-        suffLink = -1;
-        suffFLink = -1;
-        par = p;
-        symb = c;
+class TStringIterator {         //  итератор, используется для прохода по строке, при каждой итерации выводит слово                   
+private:
+    string origin;
+    string current;
+    int currIndex = 0;
+    int beginIndex = 0;
+    bool IsWord() {
+        return ((origin[currIndex]>=97) && (origin[currIndex]<=122))
+                    || (origin[currIndex] == '?');
+    }
+public:
+    bool End() {
+        return currIndex == origin.size();
+    }
+    string Next() {
+        while(!IsWord()) {
+            currIndex++;
+            if(End()) {
+                break;
+            }
+        }
+        if(origin[currIndex] != ' ') {
+            beginIndex = currIndex;
+            while(IsWord()) {
+                currIndex++;
+                if(End()) {
+                    break;
+                }
+            }
+        }
+        return origin.substr(beginIndex, currIndex - beginIndex);
+    }
+    TStringIterator(const string& s) {
+        origin = s;
     }
 };
 
 class TBohr {
 private:
+    struct TBohrVertex {
+        map<string, int> nextVertex;
+        int suffLink;
+        int suffFLink;
+        map<string, int> autoMove;
+        int par;
+        bool flag;
+        string symb;
+        int patternNumber;
+        TBohrVertex(int p, string s) {
+            flag = false;
+            suffLink = -1;
+            suffFLink = -1;
+            par = p;
+            symb = s;
+        }
+    };
     vector<TBohrVertex> Bohr;
     string Pattern;
     vector<string> PatternSplit;
+    vector<pair<string, int>> Positions;
     int GetSuffLink(int v);
-    int GetAutoMove(int v, char ch);
+    int GetAutoMove(int v, string s);
     int GetSuffFLink(int v);
     void Check(int v,int i);
     void AddString(const string& s);
+    int PatternLength(int index);
+    bool CheckPosition(int positionIndex, int patternIndex);
+    vector<int> jokerCount;      // количество джокеров между двумя подобразцами
 public:
     TBohr(const string& s);
-    bool IsStringInBohr(const string& s);
     void FindAllPos(const string& s);
 };
